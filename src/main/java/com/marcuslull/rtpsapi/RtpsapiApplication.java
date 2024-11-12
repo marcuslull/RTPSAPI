9808a4e68e047552283ff6aa7a8fabb8a41b2f0a
Marcus Lull <marcuslull@gmail.com>
2024-11-12 11:30:15 -0800

    Initial commit

---
+++ src/main/java/com/marcuslull/rtpsapi/RtpsapiApplication.java
@@ -0,0 +1,13 @@
+package com.marcuslull.rtpsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtpsapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtpsapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcuslull/rtpsapi/RtpsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuslull.rtpsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtpsapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
